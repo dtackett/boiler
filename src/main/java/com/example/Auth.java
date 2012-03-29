15c2cced1db0513b2fec8d72cae541d875a53c9a
Devon Tackett <devon.tackett@meltmedia.com>
2012-03-29 16:59:48 -0700

    Really ugly Shiro integration

---
+++ src/main/java/com/example/Auth.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+@Path("/auth")
+public class Auth {
+  
+  @POST
+  public Response login(@QueryParam("username") String username, @QueryParam("password") String password) {    
+    UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+    
+    Subject currentUser = SecurityUtils.getSubject();
+    
+    try {
+      currentUser.login(token);
+    } catch (UnknownAccountException uae) {
+//        log.info("There is no user with username of " + token.getPrincipal());
+    } catch (IncorrectCredentialsException ice) {
+//        log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+    } catch (LockedAccountException lae) {
+//        log.info("The account for username " + token.getPrincipal() + " is locked.  " +
+//                "Please contact your administrator to unlock it.");
+    }
+    // ... catch more exceptions here (maybe custom ones specific to your application?
+    catch (AuthenticationException ae) {
+        //unexpected condition?  error?
+    }    
+    
+    if (currentUser.isAuthenticated()) {
+      return Response.status(204).build();
+    } else {
+      return Response.status(401).build();
+    }
+  }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,12 +4,21 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
 @Path("/test")
 public class Main {
 
- @GET
- @Produces("text/html")
- public String getMessage( ) {
-  return "hello";
- }
+  @GET
+  @Produces("text/html")
+  public String getMessage() {
+    Subject currentUser = SecurityUtils.getSubject();
+    
+    if (!currentUser.isAuthenticated()) {
+      return "who are you?";
+    } else {    
+      return "hello " + currentUser.getPrincipal();
+    }
+  }
 }
