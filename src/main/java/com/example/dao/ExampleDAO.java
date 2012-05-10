d733604466246a917057726c435f48750f4eb0e7
Devon Tackett <devon.tackett@meltmedia.com>
2012-05-10 10:58:24 -0700

    Pagination concept work

--- src/main/java/com/example/dao/ExampleDAO.java
+++ src/main/java/com/example/dao/ExampleDAO.java
@@ -2,6 +2,8 @@ package com.example.dao;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import com.example.data.Example;
 import com.google.inject.Singleton;
 import com.google.inject.persist.Transactional;
@@ -49,5 +51,59 @@ public class ExampleDAO extends AbstractDAO<Example> {
     return super.list();
   }
   
+  private String buildSearchQuery(String search) {
+    StringBuilder qs = new StringBuilder();
+    
+    if (search != null) {
+      qs.append("where lower(e.title) like :search");
+    }
+    
+    return qs.toString();
+  }
+  
+  /**
+   * Setup the query parameters for the given query.
+   */
+  private Query setupQueryParameters(Query query, String search, Integer page, Integer pageSize) {
+    if (search != null) query.setParameter("search", search.toLowerCase());
+    
+    query.setFirstResult(page*pageSize).setMaxResults(pageSize);
+    
+    return query;
+  }
+  
+  @Transactional
+  public Long countExamples(String search) {
+    StringBuilder qs = new StringBuilder();    
+    qs.append("select count(e.id) from Example e ");
+    
+    qs.append(buildSearchQuery(search));
+    
+    Query query = provider.get().createQuery(qs.toString());
+    
+    query = setupQueryParameters(query, search, null, null);
+    
+    return (Long)query.getSingleResult();
+  }
+  
+  @Transactional
+  public List<Example> list(String search, String sort, Integer page, Integer pageSize) {
+    StringBuilder qs = new StringBuilder();    
+    qs.append("select e from Example e ");
+    
+    // Setup search query
+    qs.append(buildSearchQuery(search));
+    
+    // Setup sort
+    if (sort != null && sort.equalsIgnoreCase("title")) {
+      qs.append(" order by e.title desc");
+    }
+    
+    Query query = provider.get().createQuery(qs.toString());
+    
+    query = setupQueryParameters(query, search, page, pageSize);
+    
+    return query.getResultList();
+  }
   
 }
--- src/main/java/com/example/resource/ExampleResource.java
+++ src/main/java/com/example/resource/ExampleResource.java
@@ -5,12 +5,16 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import com.example.dao.ExampleDAO;
 import com.example.data.Example;
@@ -22,13 +26,13 @@ public class ExampleResource {
   
   @Inject ExampleDAO dao;
 
-  @GET
-  @Produces("application/json")
-  public List<Example> getExamples( ) {
-    List<Example> examples = dao.list();
-    
-    return examples;
-  }
+//  @GET
+//  @Produces("application/json")
+//  public List<Example> getExamples( ) {
+//    List<Example> examples = dao.list();
+//    
+//    return examples;
+//  }
  
   @GET
   @Path("/{exampleId}")
@@ -42,6 +46,27 @@ public class ExampleResource {
    
     return exp;
   }
+  
+  @GET
+  @Produces("application/json")
+  public Response paginateExamples( @DefaultValue("0") @QueryParam("page") Integer page, @DefaultValue("0") @QueryParam("size") Integer pageSize, @QueryParam("search") String searchParam, @QueryParam("sort") String sortField ) {
+    long pageCount = 0;
+    
+    List<Example> examples = dao.list(searchParam, sortField, page, pageSize);
+    
+    // Find the max number of pages based on the current page size
+    if (pageSize != null && pageSize > 0) {
+      pageCount = (long)Math.ceil((float)dao.countExamples(searchParam)/(float)pageSize);
+    }
+    
+    ResponseBuilder builder = Response.ok(examples);
+    
+    if (pageSize > 0) {
+      builder.header("X-pages", pageCount);
+    }
+    
+    return builder.build();    
+  }  
   
   @DELETE
   @Path("/{exampleId}")
